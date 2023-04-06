d22870150b4d22060ed3d160af98e7e6d83d58e1
Merve Nur Tasan <127309624+mervenurtasann@users.noreply.github.com>
2023-04-06 17:48:42 +0300

    week06

---
+++ src/week09/KutuphaneKayitProgrami.java
@@ -0,0 +1,12 @@
+package week09;
+
+public class KutuphaneKayitProgrami {
+    // Kitap Sinifi
+
+    static class Kitap{
+        String ad;
+        String yazarAdi;
+        String yayinEvi;
+        int yayinYili;
+    }
+}
